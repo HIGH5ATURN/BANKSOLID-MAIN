a9241994778cc8af65237b1b3bb66fdedd2fbae8
navid-kamal <navidkamal@iut-dhaka.edu>
2023-10-24 18:58:50 +0600

    created-customer-class

---
+++ Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANKSOLID
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public int NID { get; set; }
+
+        public string password { get; set; }
+
+        public Customer(string name, int nID, string password)
+        {
+            Name = name;
+            NID = nID;
+            this.password = password;
+        }
+        public void ChangePassword(string password)
+        {
+            this.password = password;
+        }
+
+    }
+}
